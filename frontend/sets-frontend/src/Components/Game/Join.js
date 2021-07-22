25fd0d27ca4dc97d72ef1aa7b1e8ac1f3f5da63b
erika <erikamanning@protonmail.com>
2021-07-22 14:52:35 -0600

    implemented create & join with code functionality
    front end displays all joined users

--- frontend/sets-frontend/src/Components/Game/Join.js
+++ frontend/sets-frontend/src/Components/Game/Join.js
@@ -1,16 +1,42 @@
-import React from "react"
+import React, { useState} from "react"
+import {useHistory} from "react-router-dom"
 
 const Join = () => {
+
+    const INITIAL_FORM = {
+        roomCode:''
+    };
+
+    const [formData,setFormData] = useState(INITIAL_FORM);
+    const history = useHistory();
+
+    const handleChange = (event) =>{
+
+        const {name,value} = event.target;
+
+        setFormData(data=>({
+            ...data,
+            [name]:value
+        }));
+    }
+
+    const handleSubmit = async (event) =>{
+
+        event.preventDefault();
+        alert('Form submitted!');
+        setFormData(INITIAL_FORM);
+        history.push(`/lobby/${formData.roomCode}`);
+    }
 
     return <div>
 
         <h1 className='mt-5' >Join a game!</h1>
         <div className="row justify-content-center mt-5">
             <div className="col-12 col-lg-3">
-                <form action='/lobby'>
+                <form onSubmit={handleSubmit} action='/lobby'>
                     <div className="mb-3">
-                        <label for="roomCode" className="form-label">Enter the room code here:</label>
-                        <input type="text" className="form-control text-center" id="roomCode" aria-describedby="roomCode"/>
+                        <label htmlFor="roomCode" className="form-label">Enter the room code here:</label>
+                        <input name='roomCode' type="text" value={formData.roomCode} onChange={handleChange} className="form-control text-center" id="roomCode" aria-describedby="roomCode"/>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
