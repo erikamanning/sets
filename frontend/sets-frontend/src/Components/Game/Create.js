7cca3020300dc476df96dd74bdd5d1930386e4fe
erika <erikamanning@protonmail.com>
2021-08-03 14:22:25 -0600

    guest user no longer enters name for single player  mode

--- frontend/sets-frontend/src/Components/Game/Create.js
+++ frontend/sets-frontend/src/Components/Game/Create.js
@@ -3,15 +3,37 @@ import {useSelector} from "react-redux"
 import GameRoom from './GameRoom'
 import GuestIdForm from "./GuestIdForm.js"
 import * as Colyseus from 'colyseus.js';
+import {getRandomIntInclusive} from './RoomHelpers'
 
 const Create = ({mode='sets_multiplayer'}) => {
     
+    // if user logged in, set username to logged in username
+
+    // else if multiplayer, render form to create username
+    
+    // otherwise render gameroom with no username, 
+        // since it does not matter at all in any way shape or form
+
+
     let client;
     const user = useSelector(state=>state.user);
+
+    console.log('CREATE -- user: ', user.username);
+
     const [room,setRoom] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(user.username ? true :false);
     const [guestUser, setGuestUser] = useState(false);
 
     useEffect(()=>{
+
+        if(mode!=='sets_multiplayer' && !loggedIn){
+            setGuestUser(`guest${getRandomIntInclusive(1000,9999)}`);
+        }
+
+    },[]);
+
+    useEffect(()=>{
+
         const createRoom = async (mode,userId) => {
             client = new Colyseus.Client('ws://localhost:5000');
 
@@ -46,7 +68,7 @@ const Create = ({mode='sets_multiplayer'}) => {
                         : <h1>Create a New Game</h1>
                 }
                 {
-                    !user.username && !guestUser
+                    !loggedIn && mode==='sets_multiplayer'
                         ?  <GuestIdForm addGuest={addGuest}/>
                         : null
                 }
