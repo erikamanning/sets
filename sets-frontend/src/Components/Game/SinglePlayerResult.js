1176882b900bad77465a20caec207910caf03d3b
erika <erikamanning@protonmail.com>
2021-10-13 11:12:23 -0600

    Added single player result component and displayed score

--- sets-frontend/src/Components/Game/SinglePlayerResult.js
+++ sets-frontend/src/Components/Game/SinglePlayerResult.js
@@ -3,6 +3,10 @@ import GameContext from './GameContext'
 
 const SinglePlayerResult = () => {
 
+    const {scoreboard} = useContext(GameContext);
+
+    console.log('Score: ', scoreboard.entries().next().value[1].score);
+
     return (
         
         <div className="container mt-5">
@@ -10,7 +14,7 @@ const SinglePlayerResult = () => {
                 <div className='col-12 col-lg-6 border border-5 border-primary rounded p-5'>
                     <h1 className='display-1 text-danger'> Results</h1>
                     <div class="d-grid gap-2">
-                        Score:                 
+                        <p className='display-5 text-primary'>Score: {scoreboard.entries().next().value[1].score}</p>            
                         <a className='btn btn-lg btn-primary me-1' href='/singleplayer'>Play Again?</a>
                         <button className='btn btn-lg btn-outline-primary border-3' >Leave</button>
                     </div>
